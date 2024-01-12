5dd1e882d68cba7b9304e5c2af2eab4970e8eeba
leandroGmaisP <leandro@gmaisp.com.br>
2024-01-11 22:33:13 -0300

    first commit

---
+++ prisma/migrations/20240111171523_tete1/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "info" TEXT,
+    "role" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,142 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    console.log("------------ Main Iciando - connectd ------------")
+    console.log("executando")
+    // Crud com Prisma
+// ---------------- Create usuario -------------------
+//    const user = await prisma.user.create({
+//         data: {
+//             email: "Jasmim@thtc.com",
+//             name: "Jasmim",
+//             password: "1234"
+//         }
+//     })
+
+//     console.dir(user, {depth: null})
+// }
+
+// --------------- delete usuario -------------------
+// const userDeleted = await prisma.user.delete({
+//     where: {
+//         id: "67bc52e0-c4c5-45e1-97fa-f8059e72fda6"
+//     }
+// })
+
+//     console.dir(userDeleted, {depth: null})
+
+// ---------------- update usuario -------------------
+//  const userUpdate = await prisma.user.update({
+//     where:{
+//         id: "0d341af6-3c5d-4383-bb37-8ac5a403e45b"
+//     },
+//     data:{
+//         email: "leandro@novoEmail.com"
+//     }
+// })
+
+// ---------------- Leitura de um  usuario -------------------
+// const leandro = await prisma.user.findUnique({
+//     where:{
+//         id: "0d341af6-3c5d-4383-bb37-8ac5a403e45b"
+//     }
+// })
+
+// console.dir(leandro, {depth: null})
+
+// ---------------- Listando posts do usuario -------------------
+// const users = await prisma.user.findMany({
+//     include:{
+//         posts: true,
+//         profile: true
+//     }
+// })
+// console.dir(users, {depth: null})
+
+
+// ---------------- Buscas usando select -------------------
+
+// const busca = await prisma.user.findMany({
+//     select:{
+//         email: true
+//     }
+// })
+// console.log(busca);
+// ---------------- Criando relacionamentos -------------------
+
+// const leandro = await prisma.user.create({
+//     data:{
+//         name:"Leandro Arraes",
+//         email:"leandroArraes@thtc.com",
+//         password: "1234",
+//         profile:{
+//             create:{
+//                 info:"Ola Mundo",
+//                 role:"admin",
+//             }
+//         },
+//         posts:{
+//             create:{
+//                 title:"Agora foi",
+//                 content:"Ola Mundo",
+//                 published: true
+//             }
+//         }
+// }})
+//console.dir(leandro, {depth: null})
+
+// --------------- Deletando com relacionamento -------------------
+
+
+    apagarUsusario('67bc52e0-c4c5-45e1-97fa-f8059e72fda6');
+
+
+// ------- função para mostrar todos os usuarios --------------
+// const MostrarTodosUsuarios = await prisma.user.findMany();
+// console.dir(MostrarTodosUsuarios, {depth: null})
+
+// --------- Usando filtros --------------------
+// const users = await prisma.user.findMany(
+//     {where: {name: {contains: "leandro"}}}
+// );
+
+//     console.dir(users, {depth: null})
+
+
+
+}
+
+async function apagarUsusario(ids: string){
+    await prisma.post.deleteMany({
+        where:{
+            authorId: ids
+        }
+    })
+
+    await prisma.profile.delete({
+        where:{
+            userId: ids
+        }
+    })
+
+    await prisma.user.delete({
+        where: {
+          id: ids
+        }
+      });
+
+    console.log("Usuaio deletado")
+};
+
+
+main().then(async(e)=>{
+    await prisma.$disconnect();
+    console.log("Disconectar")
+}).catch(async(e)=>{
+    console.log(e)
+    await prisma.$disconnect();
+    console.log("Disconectar")
+    process.exit(1)
+})
